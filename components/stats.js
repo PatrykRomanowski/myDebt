f882b04cca6c4d98afa59a12c600a93cdb2dda2b
PatrykRomanowski <patrykromanowski90@gmail.com>
2024-11-17 16:19:09 +0100

    changes for stats v2

--- components/stats.js
+++ components/stats.js
@@ -5,6 +5,7 @@ import {
   TouchableOpacity,
   StyleSheet,
   Dimensions,
+  Modal,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -14,6 +15,10 @@ const screenWidth = Dimensions.get("window").width;
 const Stats = () => {
   const [allData, setAllData] = useState([]);
   const [toogleData, setToogleData] = useState(false);
+  const [modalForDelete, setModalForDelete] = useState(false);
+  const [itemForDelete, setItemForDelete] = useState(null);
+  const [openItemInfo, setOpenItemInfo] = useState(false);
+
   const readData = async () => {
     try {
       const jsonValue = await AsyncStorage.getItem("statsDataForDebit");
@@ -37,12 +42,11 @@ const Stats = () => {
     }
   };
 
-  const deleteItem = async (indexForDelete) => {
-    const newArray = allData.filter((_, index) => index !== indexForDelete);
+  const deleteItemAfterConfirmation = () => {
+    const newArray = allData.filter((_, index) => index !== itemForDelete);
     setAllData(newArray);
-    console.log(indexForDelete);
+    // console.log(indexForDelete);
     setToogleData(!toogleData);
-
     const saveData = async () => {
       try {
         const jsonValue = JSON.stringify(newArray);
@@ -52,12 +56,22 @@ const Stats = () => {
       }
     };
     saveData();
+    setModalForDelete(false);
+  };
+
+  const deleteItem = async (indexForDelete) => {
+    setModalForDelete(true);
   };
 
   const editItem = async (indexForDelete) => {
     // const newArray = allData.filter((_, index) => index !== indexForDelete);
     // setAllData(newArray);
     console.log(indexForDelete);
+  };
+
+  const infoModal = async (indexForDelete) => {
+    setItemForDelete(indexForDelete);
+    setOpenItemInfo(true);
   };
 
   const addDataForAsyncStore = async () => {
@@ -96,45 +110,120 @@ const Stats = () => {
 
   return (
     <View style={styles.containerForAllData}>
-      <Text>Saved Data:</Text>
+      <View>
+        <Text style={styles.textDescription}>ALL DEBITS</Text>
+      </View>
+      <TouchableOpacity onPress={addDataForAsyncStore}>
+        <Text>Add New Data</Text>
+      </TouchableOpacity>
       {allData.length > 0 ? (
         allData.map((item, index) => (
-          <View style={styles.containerForItem} key={index}>
-            <View>
-              <Text>{item.name}</Text>
-            </View>
-            <View>
-              {/* <Text>{item.data}</Text> */}
-              <Text>{item.value}</Text>
-            </View>
-            <View>
-              <TouchableOpacity onPress={() => deleteItem(index)}>
+          <TouchableOpacity onPress={() => infoModal(index)}>
+            <View style={styles.containerForItem} key={index}>
+              <View>
+                <Text style={styles.nameText}>{item.name}</Text>
+              </View>
+              <View style={styles.containerForValueTrashEdit}>
+                <View>
+                  {/* <Text>{item.data}</Text> */}
+                  <Text style={styles.valueText}>{item.value} PLN</Text>
+                </View>
+                {/* <View>
+                <TouchableOpacity onPress={() => deleteItem(index)}>
                 <Ionicons name="create-outline" color={"black"} size={22} />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => deleteItem(index)}>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => deleteItem(index)}>
                 <Ionicons name="trash" color={"black"} size={22} />
-              </TouchableOpacity>
+                </TouchableOpacity>
+                </View> */}
+              </View>
             </View>
-          </View>
+          </TouchableOpacity>
         ))
       ) : (
         <Text>No data available</Text>
       )}
-      <TouchableOpacity onPress={addDataForAsyncStore}>
-        <Text>Add New Data</Text>
-      </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={openItemInfo}
+        onRequestClose={() => setOpenItemInfo(false)}
+      >
+        <Text>XD</Text>
+      </Modal>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalForDelete}
+        onRequestClose={() => setModalForDelete(false)}
+      >
+        <View style={styles.modalBackground}>
+          <Text>CZY NAPENO CHCESZ USUNĄĆ WARTOŚĆ?</Text>
+          <TouchableOpacity
+            onPress={() => {
+              deleteItemAfterConfirmation();
+            }}
+          >
+            <Text>TAK</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              setModalForDelete(false);
+            }}
+          >
+            <Text>NIE</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   containerForAllData: {
-    width: screenWidth * 0.9,
+    // width: screenWidth * 0.9,
     justifyContent: "center",
     alignItems: "center",
     margin: 10,
   },
-  containerForItem: {},
+  containerForItem: {
+    width: screenWidth * 0.95,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 20,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+  },
+  containerForValueTrashEdit: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 5,
+  },
+  valueText: {
+    fontFamily: "Inter_500Medium",
+    fontSize: 18,
+    marginRight: 20,
+  },
+  nameText: {
+    fontFamily: "Inter_400Regular",
+    marginLeft: 20,
+    textTransform: "uppercase",
+  },
+  textDescription: {
+    fontFamily: "Inter_600SemiBold",
+    fontSize: 16,
+    textTransform: "uppercase",
+  },
 });
 
 export default Stats;
